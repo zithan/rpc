d17f330a05ce299e282a7d25e542c892fc0427e1
hutong <hutong0521@163.com>
2018-06-04 21:59:11 +0800

    修改

---
+++ app/server/Common/Version.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by zithan.
+ * User: zithan <zithan@163.com>
+ */
+
+namespace App\Server\Common;
+
+use App\Server\Base;
+
+class Version extends Base
+{
+    /**
+     * 检测版本升级情况
+     *
+     * @return [type] [description]
+     * @author hutong
+     * @date   2018-06-04T21:50:54+080
+     */
+    public function check()
+    {
+
+    }
+}
