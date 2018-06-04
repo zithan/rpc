957940803247766c0593860d5f7bfc28823fa74a
hutong <hutong0521@163.com>
2018-06-04 21:57:11 +0800

    修改

---
+++ app/server/Common/Favorites.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by zithan.
+ * User: zithan <zithan@163.com>
+ */
+
+namespace App\Server\Common;
+
+use App\Server\Base;
+
+class Favorites extends Base
+{
+    /**
+     * 收藏商品列表
+     *
+     * @return [type] [description]
+     * @author hutong
+     * @date   2018-06-04T21:50:54+080
+     */
+    public function goods()
+    {
+
+    }
+
+    /**
+     * 收藏品牌列表
+     * 
+     * @return [type] [description]
+     * @author hutong
+     * @date   2018-06-04T21:55:31+080
+     */
+    public function brand()
+    {
+
+    }
+
+    /**
+     * 收藏操作
+     *  收藏商品、收藏品牌
+     *
+     * @return [type] [description]
+     * @author hutong
+     * @date   2018-06-04T21:53:53+080
+     */
+    public function toggle()
+    {
+
+    }
+}
---
+++ app/server/Common/History.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by zithan.
+ * User: zithan <zithan@163.com>
+ */
+
+namespace App\Server\Common;
+
+use App\Server\Base;
+
+class History extends Base
+{
+    /**
+     * 商品足迹
+     *
+     * @return [type] [description]
+     * @author hutong
+     * @date   2018-06-04T21:50:54+080
+     */
+    public function list()
+    {
+
+    }
+
+    /**
+     * 添加商品足迹
+     *
+     * @return [type] [description]
+     * @author hutong
+     * @date   2018-06-04T21:55:31+080
+     */
+    public function add()
+    {
+
+    }
+}
